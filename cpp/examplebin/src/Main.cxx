0979086d08b9bb4dce727517bc29856f84715db7
Bram Tukker <bram.tukker@hva.nl>
2018-09-11 11:33:51 +0200

    Added curl library

--- cpp/examplebin/src/Main.cxx
+++ cpp/examplebin/src/Main.cxx
@@ -1,6 +1,31 @@
+#include <curl/curl.h>
 #include <iostream>
 #include "Foo.h"
 
 int main(int argc, char** argv) {
-    std::cout << Foo::bar("test") << "\n";
+    char input;
+
+    std::cout << Foo::bar("Make request? ");
+    std::cin >> input;
+    
+    if (input == 'y') {
+        CURL *curl;
+        CURLcode res;
+
+        curl = curl_easy_init();
+        if(curl) {
+            curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
+            curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+            res = curl_easy_perform(curl);
+
+            if(res != CURLE_OK) {
+                fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+            }
+
+            curl_easy_cleanup(curl);
+        }
+    }
+    
+    return 0;
 }
