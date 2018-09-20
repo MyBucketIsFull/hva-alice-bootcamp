31fd7227ff92811efae56eee9df1743231e98cd7
Bram Tukker <bram.tukker@hva.nl>
2018-09-20 22:50:46 +0200

    C++ build #16

--- cpp/CMakeModules/FindZookeeper.cmake
+++ cpp/CMakeModules/FindZookeeper.cmake
@@ -3,9 +3,11 @@ cmake_minimum_required(VERSION 2.8)
 include(FindPackageHandleStandardArgs)
 
 include_directories(/opt/zookeeper-3.4.12/src/c/include/)
-
-find_library(ZOOKEEPER_LIBRARY zookeeper_mt)
-find_path(ZOOKEEPER_INCLUDE_DIR "zookeeper/zookeeper.h")
+find_library(
+    ZOOKEEPER_LIBRARY
+    zookeeper_mt
+    HINTS "/opt/zookeeper-3.4.12/src/c/include/")
+find_path(ZOOKEEPER_INCLUDE_DIR "zookeeper.h")
 
 set(
     ZOOKEEPER_LIBRARIES
